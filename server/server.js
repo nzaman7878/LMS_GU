99cb2684a36032b6c74230dc18991abacb60b285
nzaman7878 <nuruzzaman31032001@gmail.com>
2026-02-12 01:16:12 +0530

    connect backend to MongoDB database

--- server/server.js
+++ server/server.js
@@ -1,13 +1,21 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js"
+
+
 
 dotenv.config(); 
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+connectDB();
 app.use(express.json());
 
 app.listen(port, () => {
   console.log(` Server started on port ${port}`);
+});
+
+app.get("/", (req, res) => {
+  res.send("Server is running ");
 });

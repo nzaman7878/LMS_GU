53f20eac7dd5e37227504571250e8d86a4a7b3c7
nzaman7878 <nuruzzaman31032001@gmail.com>
2026-02-14 12:08:40 +0530

    implement teacher authentication middleware using JWT

---
+++ server/middleware/authTeacher.js
@@ -0,0 +1,40 @@
+import jwt from "jsonwebtoken";
+
+const authTeacher = async (req, res, next) => {
+  try {
+    const { ttoken } = req.headers;
+
+   
+    if (!ttoken) {
+      return res.status(401).json({
+        success: false,
+        message: "Unauthorized: No token provided",
+      });
+    }
+
+    
+    const decoded = jwt.verify(ttoken, process.env.JWT_SECRET);
+
+   
+    req.teacherId = decoded.id;
+
+    next();
+
+  } catch (error) {
+    console.log(error);
+
+    if (error.message === "jwt expired") {
+      return res.status(401).json({
+        success: false,
+        message: "Token expired",
+      });
+    }
+
+    res.status(401).json({
+      success: false,
+      message: "Invalid token",
+    });
+  }
+};
+
+export default authTeacher;

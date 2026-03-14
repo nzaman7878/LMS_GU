81a19738b08660b35ec6640bc94c5ad19d19a843
nzaman7878 <nuruzzaman31032001@gmail.com>
2026-03-14 23:57:44 +0530

    Enhance CourseList component; implement search functionality and dynamic course filtering

--- client/src/page/student/CourseList.jsx
+++ client/src/page/student/CourseList.jsx
@@ -1,18 +1,37 @@
-import { useContext } from "react"
+import { useContext, useEffect, useState } from "react"
 import { AppContext } from "../../context/AppContext"
 import SearchBar from "../../components/students/SearchBar"
 import { useParams } from "react-router-dom"
 import CourseCard from "../../components/students/CourseCard"
+import { assets } from "../../assets/assets"
+import Footer from "../../components/students/Footer"
 
 const CourseList = () => {
 
   const { navigate, allcourses } = useContext(AppContext)
   const { searchTerm } = useParams()
 
+  const [filteredCourses, setFilteredCourses] = useState([])
+
+  useEffect(() => {
+
+    if (searchTerm) {
+      const results = allcourses?.filter((course) =>
+        course.courseTitle.toLowerCase().includes(searchTerm.toLowerCase())
+      )
+
+      setFilteredCourses(results)
+
+    } else {
+      setFilteredCourses(allcourses)
+    }
+
+  }, [searchTerm, allcourses])
+
   return (
+    <>
     <div className="md:px-36 px-8 pt-20">
 
-      
       <div className="flex md:flex-row flex-col gap-6 items-start md:items-center justify-between">
 
         <div>
@@ -34,17 +53,27 @@ const CourseList = () => {
         <SearchBar data={searchTerm} />
 
       </div>
-
-      
+        {
+          searchTerm && <div className="inline-flex items-center gap-4 px-4 py-2 border mt-8 mb-8 text-gray-600">
+            <p>{searchTerm}</p>
+            <img src={assets.cross_icon} alt=""
+            className="cursor-pointer" onClick={()=> 
+              navigate("/course-list")
+            } />
+          </div>
+        }
       <div className="grid sm:grid-cols-2 lg:grid-cols-4 gap-6 mt-10">
 
-        {allcourses?.map((course) => (
+        {filteredCourses?.map((course) => (
           <CourseCard key={course._id} course={course} />
         ))}
 
       </div>
+      
 
     </div>
+    <Footer />
+    </>
   )
 }
 
